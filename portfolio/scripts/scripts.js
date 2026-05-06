54b83a7f208741a8e9a18d9b96ce5dfdef59da0d
Thiago Henrique Sousa Melo <thiagohs.dev@gmail.com>
2026-05-06 18:31:28 +0000

    adcionando codicionais e switch case

--- portfolio/scripts/scripts.js
+++ portfolio/scripts/scripts.js
@@ -2,6 +2,16 @@ const NOME = "Thiago Henrique";
 let tituloProfissional = "Desenvolvedor Backend";
 let minhaBio = "Estudante de Desenvolvimento de Sistemas com foco em back-end, desenvolvendo habilidades em Python, Javascript e banco de dados. Comprometido com a resolução de problemas e a criação de soluções eficientes através da prática constante.studante de desenvolvimento de sistemas, com foco em backend, muito esforçado e dedicado.";
 
+
+
+let indefinido;
+let nulo = null;
+let curso = {
+    nome: "Desenvolvimento de Sistemas",
+        semestre: 3,
+            disciplinaAtual: "Linguagem de Programação"
+            };
+
 let anoFormatura = 2026;
 let mesFormatura = 12;
 let diaFormatura = 31;
@@ -25,14 +35,6 @@ let anos = anoFormatura - anoAtual;
 let meses = mesFormatura - mesAtual;
 let dias = diaFormatura - diaAtual;
 
-let ajusteDias = (dias < 0) * 1;
-dias = dias + (30 * ajusteDias);
-meses = meses - ajusteDias;
-
-
-let ajusteMeses = (meses < 0) * 1;
-meses = meses + (12 * ajusteMeses);
-anos = anos - ajusteMeses;
 
 
 
@@ -40,26 +42,45 @@ anos = anos - ajusteMeses;
 
 
 
-let indefinido;
-let nulo = null;
-let curso = {
-    nome: "Desenvolvimento de Sistemas",
-    semestre: 3,
-    disciplinaAtual: "Linguagem de Programação"
-};
 
 
-console.log(typeof nulo);
-console.log(typeof indefinido);
-console.log(typeof anoFormatura);
-console.log(typeof minhaBio);
-console.log(typeof tituloProfissional);
-console.log(typeof NOME);
-console.log(typeof curso);
+
+
+if (meses <= 0 && dias <= 0 && anos <= 0) {
+    document.getElementById("tempoRestante").innerText = 'Curso já concluído';
+}
+if (anoFormatura - anoAtual <= 0) {
+    document.getElementById("tempoRestante").innerText = '-';
+} else if (anoFormatura - anoAtual === 1) {
+    document.getElementById("tempoRestante").innerText = `Tempo restante: ${anoFormatura - anoAtual} ano`;
+} else {
+    document.getElementById("tempoRestante").innerText = `Tempo restante: ${anoFormatura - anoAtual} anos`;
+}
+
+
 
 document.getElementById("meuNome").innerText = NOME;
 document.getElementById("tituloProfissional").innerText = tituloProfissional;
 document.getElementById("minhaBio").innerText = minhaBio;
 document.getElementById("anoFormatura").innerText = "Ano de formatura: " + anoFormatura;
-document.getElementById("tempoRestante").innerText =
-`Faltam ${anos} anos, ${meses} meses e ${dias} dias para sua formatura.`;
+document.write(`<p> Ano de Ingresso: ${anoIngresso}</p>`);
+
+
+let diaSemana = hoje.getDay() + 1;
+
+let diaescrito;
+
+switch (diaSemana) {
+    case 1: diaescrito = "Segunda-feira"; break;
+    case 2: diaescrito = "Terça-feira"; break; 
+    case 3: diaescrito = "Quarta-feira"; break;
+    case 4: diaescrito = "Quinta-feira"; break;
+    case 5: diaescrito = "Sexta-feira"; break;
+    case 6: diaescrito = "Sábado"; break;
+    case 7: diaescrito = "Domingo"; break;
+    default: diaescrito = "Dia inválido"; 
+
+}
+
+
+document.write(`<p> Hoje é ${diaescrito} </p>`);
